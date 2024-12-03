b629e7302c5ce21212ff8b34580987d9498d9412
javier <javier.sepulveda.verdugo@gmail.com>
2024-12-03 21:29:59 +0100

    feat: handle longer titles

--- src/evangelion/TitlePage.tsx
+++ src/evangelion/TitlePage.tsx
@@ -3,9 +3,10 @@ import { Box } from "@chakra-ui/react";
 export const TitlePage: React.FC = () => {
   // read query params: title1, title2, title3
   const urlParams = new URLSearchParams(window.location.search);
-  const title1 = urlParams.get("title1");
+  const title1 = urlParams.get("title1") || "TAIPEI";
   const title2 = urlParams.get("title2");
   const title3 = urlParams.get("title3");
+  const isLongTitle = title1.length > 9;
   return (
     <Box
       sx={{
@@ -23,11 +24,11 @@ export const TitlePage: React.FC = () => {
           sx={{
             color: "white",
             height: "6rem",
-            fontSize: "4rem",
+            fontSize: isLongTitle ? "3.3rem" : "4rem",
             letterSpacing: "0.1rem",
           }}
         >
-          <Box sx={{ transform: "scaleY(2)" }}>{title1 || "TAIPEI"}</Box>
+          <Box sx={{ transform: isLongTitle ? "scaleY(1.5)" : "scaleY(2)" }}>{title1}</Box>
         </Box>
         <Box
           sx={{
@@ -41,11 +42,16 @@ export const TitlePage: React.FC = () => {
         </Box>
         <Box
           sx={{
+            position: "absolute",
+            bottom: "20rem",
+            right: 0,
             color: "white",
             fontSize: "3rem",
             fontFamily: "Noto Sans, sans-serif",
             textAlign: "end",
             marginTop: "1rem",
+            writingMode: "tb-rl",
+            transform: "rotate(-180deg)",
           }}
         >
           {title3 || "Day 1"}
